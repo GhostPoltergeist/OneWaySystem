bebd511cf461e7fbe55c30df384257b113cc6a35
GhostPoltergeist <Edselcabaluna21@gmail.com>
2022-06-23 13:15:53 -0800

    I've deleted the old files to replace it with new latest java files

--- Database.java
+++
@@ -1,9 +0,0 @@
-package CodingwithHarold.Flight;
-
-public class Database {
-    private static final int cebuCity = 1938;
-    private static final int elNido = 5683;
-    private static final int davaoCity = 2717;
-    private static final int puertoPrincesa = 2538;
-    private static final int airAsia = 2717;
-}
